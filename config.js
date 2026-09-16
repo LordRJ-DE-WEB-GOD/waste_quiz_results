61b0628b095591e4c7eefbd418456ccc3d6a27fb
Rhulani.RJ <marhulir@gmail.com>
2026-09-16 20:50:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-// ==========================================
-// SUPABASE CONFIGURATION
-// Waste Management Campus Survey
-// ==========================================
+// ============================================================
+// ECOPULSE — SUPABASE CONFIGURATION
+// WASTE MANAGEMENT CAMPUS SURVEY
+// ============================================================
 
 const SUPABASE_URL = "https://lvsflentewfofxkldjnv.supabase.co";
 
 const SUPABASE_ANON_KEY =
     "sb_publishable_yXs_49ClfcNZ-lcXlsB6LQ_puN_ssXh";
+
+const TABLE_NAME = "waste_management_responses";
